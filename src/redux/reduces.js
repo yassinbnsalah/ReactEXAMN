4dc6e05708c76845529243aa8a483dbc0e717f2d
yassinebnsalah <yacinbnsalh@gmail.com>
2024-05-13 23:25:18 +0100

    first commit

--- src/redux/reduces.js
+++ src/redux/reduces.js
@@ -1,10 +1,7 @@
 import { combineReducers } from "redux";
-import movies from "./moviesSlice.js";
-import wishlist from "./wishlistSlice.js";
 import Data from './dataSlice.js'
 const reducers = combineReducers({
-    movies,
-    wishlist,Data
+    Data
 });
 
 export default reducers;
